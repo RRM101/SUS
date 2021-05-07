b4d2f5f5b20ff67bd625d8bce08688e6a739ef53
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2021-05-07 19:42:51 +0530

    Add files via upload

---
+++ Debian sus.sh
@@ -0,0 +1,10 @@
+cd
+sudo apt install git
+git clone https://github.com/RickRollMaster101/SUS.git
+sudo apt install vlc
+cd SUS
+vlc 'SUS.mp4'
+sudo apt remove vlc
+sudo apt purge vlc
+cd ..
+rm -rf SUS
